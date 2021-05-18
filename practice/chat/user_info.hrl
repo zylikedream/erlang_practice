f178daf2d6299c42f9164598f28b74a056f9b661
zyr <zy19910126@126.com>
2021-05-18 17:17:20 +0800

    删除无用文件

--- practice/chat/user_info.hrl
+++
@@ -1,9 +0,0 @@
--record(user, {
-        id        %% 用户ID
-        ,name     %% 用户名称
-        ,passwd   %% 用户登录密码
-        ,login_times %% 登录次数
-        ,chat_times  %% 聊天次数
-        ,last_login  %% 最后一次登录时间
-        ,state  %% 登录状态  0 未登录  1已登录
-    }).
